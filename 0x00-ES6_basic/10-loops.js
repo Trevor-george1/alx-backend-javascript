5d588be5a2dff39ff7ce58c08829720b64485f90
Trevor <Trevormahinda@gmail.com>
2024-07-24 20:11:34 +0300

    loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    const new_array = []
+    for (const idx of array) {
+        new_array.push(appendString + idx)
+    }
+  
+    return new_array;
+  }
