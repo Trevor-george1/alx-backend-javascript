cf1cf032000b1b269b239a3d45dcf5379eba2e22
Trevor <Trevormahinda@gmail.com>
2024-07-25 17:16:26 +0300

    all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import { uploadPhoto, createUser } from "./utils.js";
+export default async function handleProfileSignup() {
+    const photo = await uploadPhoto()
+    const create = await createUser()
+
+    return Promise.all([photo, create]).then((res) => {
+        console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch((error) => {
+        console.log("Signup system offline");
+    });
+}
