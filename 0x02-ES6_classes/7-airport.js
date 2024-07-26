01cdaadbe8bafb1667a64a899fa8b09a00afff11
Trevor <Trevormahinda@gmail.com>
2024-07-26 22:13:43 +0300

    airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -6,6 +6,22 @@ export default class Airport {
         this._code = code;
     }
 
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    set code(value) {
+        this._code = code;
+    }
+
     toString() {
         return `[object ${this._code}]`;
     }
